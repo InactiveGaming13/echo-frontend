e6890a038afed4c015673977122bbe89ea9f7f51
InactiveGaming13 <116043268+InactiveGaming13@users.noreply.github.com>
2025-02-11 15:09:02 +1000

    Convert the project to use Flask and Jinja2

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, render_template as renderTemplate, request, redirect, url_for
+from flask import Flask, Blueprint, render_template as renderTemplate, request, redirect, url_for as urlFor
 from flask_socketio import SocketIO, emit
 from flask_sse import sse
 
@@ -8,7 +8,6 @@ app = Flask(__name__)
 # Configure the Flask app
 app.config["SECRET_KEY"] = "UBF)*(VB)PD:CV^6gf8evf8y0o6asfCG*VO*)^vfc"
 app.config["REDIS_URL"] = "redis://localhost"
-app.config["TEMPLATES_FOLDER"] = "../templates"
 
 # Create the Flask blueprints
 static: Blueprint = Blueprint("static", __name__, static_folder="static", static_url_path="/static")
