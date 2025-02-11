eb09b67e4eb057c3cda2010c4c92a45f79a159b7
InactiveGaming13 <116043268+InactiveGaming13@users.noreply.github.com>
2025-02-11 17:01:33 +1000

    Create sse.js

---
+++ static/js/sse.js
@@ -0,0 +1,10 @@
+let source = new EventSource("{{ url_for('sse.stream') }}");
+
+source.addEventListener("message", function(event) {
+    let data = JSON.parse(event.data);
+    alert(data.message);
+}, false);
+
+source.addEventListener("error", function(event) {
+    console.error("Failed to connect to event stream. Is the server running?");
+}, false);
