76c3ab330539a44748a3c9216fc2d905995c4d67
InactiveGaming13 <116043268+InactiveGaming13@users.noreply.github.com>
2025-02-11 14:55:45 +1000

    Remove the server directory

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, Blueprint, render_template as renderTemplate, request, redirect, url_for
+from flask_socketio import SocketIO, emit
+from flask_sse import sse
+
+# Create the Flask app
+app = Flask(__name__)
+
+# Configure the Flask app
+app.config["SECRET_KEY"] = "UBF)*(VB)PD:CV^6gf8evf8y0o6asfCG*VO*)^vfc"
+app.config["REDIS_URL"] = "redis://localhost"
+app.config["TEMPLATES_FOLDER"] = "../templates"
+
+# Create the Flask blueprints
+static: Blueprint = Blueprint("static", __name__, static_folder="static", static_url_path="/static")
+
+# Register the Flask blueprints
+app.register_blueprint(sse, url_prefix="/stream")
+app.register_blueprint(static, url_prefix="/static")
+
+# Create the SocketIO object
+socketio = SocketIO(app)
+
+
+# Define the route for the index page
+@app.route("/")
+def index():
+    return renderTemplate("index.html")
+
+
+# Start the Flask app
+if __name__ == "__main__":
+    socketio.run(app, host="127.0.0.1", port=5000, allow_unsafe_werkzeug=True)
